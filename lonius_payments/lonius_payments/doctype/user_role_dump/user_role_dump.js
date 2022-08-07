0de994823f25e4d5ec9a992e5e0d26837b6e6a48
Baba Zaha <dsmwaura@gmail.com>
2022-08-07 20:04:59 +0300

    Lonius subscription checker v1

---
+++ lonius_payments/lonius_payments/doctype/user_role_dump/user_role_dump.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Lonius Devs and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('User Role Dump', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ lonius_payments/lonius_payments/doctype/user_role_dump_item/test_user_role_dump_item.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Lonius Devs and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestUserRoleDumpItem(unittest.TestCase):
+	pass
